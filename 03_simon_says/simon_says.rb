06db77a056af18185204d1dcb05ad46398f233c5
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-10-29 23:32:41 +0200

    Solve exercise 3

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
 #write your code here
+def echo(s)
+  s
+end
+
+def shout(s)
+  s.upcase
+end
+
+def repeat(s, n = 2)
+  [s] * n * ' '
+end
+
+def start_of_word(s, n)
+  s[0...n]
+end
+
+def first_word(s)
+  s.split.first
+end
+
+def titleize(words)
+  little_words = %w{and or the over a an for nor of}
+  words.split.each_with_index{|word, i| word.capitalize! unless little_words.include?(word) && i > 0} * ' '
+end

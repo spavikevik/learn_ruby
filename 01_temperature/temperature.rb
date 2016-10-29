84d3f862d9941d16e5d7f8a4c89cb74c43e88bc3
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-10-29 15:00:50 +0200

    Solve exercises 0, 1 and 2

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(t)
+  (t-32) * 5.0/9
+end
+
+def ctof(t)
+  t*9.0/5+32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(a, b)
+  a+b
+end
+
+def subtract(a, b)
+  a-b
+end
+
+def sum(l)
+  l.reduce(0, :+)
+end
+
+def multiply(l)
+  l.reduce(1, :*)
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(n)
+  n > 1 ? n * factorial(n-1) : 1
+end

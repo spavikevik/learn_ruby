1e66338643b6bf150e9e3bafe953bb77d3c13ceb
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-10-30 12:50:34 +0100

    Solve exercise 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,8 @@
 #write your code here
+def translate(words)
+  regex = '\A([(?:qu)||[^aeiouAEIOU]]*)(.*)\z'
+  words.split.each do |word|
+    word.match regex
+    word.replace "#{$2}#{$1}ay"
+  end * ' '
+end

f2faae21e963531c1e8ad8e8c827733b61c6d16b
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-10-30 13:03:20 +0100

    Solve exercise 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,11 @@
 class Book
 # write your code here
+  attr_writer :title
+
+  def title
+    ignore_words = %w{the a an and in on of at or}
+    @title.split.each_with_index do |word, n|
+      word.capitalize! unless ignore_words.include?(word) && n > 0
+    end * ' '
+  end
 end
